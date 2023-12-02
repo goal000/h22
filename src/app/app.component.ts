d1104c1819002d7077d1af3fb10bac9aa69f83d5
WellWisher <118476290+goal000@users.noreply.github.com>
2023-12-02 11:51:04 +0530

    f1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { FormGroup,FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  registrationform=new FormGroup(
+    {
+      username:new FormControl(''),
+      password:new FormControl(''),
+      cfnpassword:new FormControl('')
+    }
+);
+
+loginrequest() {
+  console.log(this.registrationform.value)
+}
   title = 'a12';
+
 }
